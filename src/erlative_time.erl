6fbca074ca4346a6615dba9910a1720d661da522
John Haugeland <stonecypher@gmail.com>
2015-06-29 23:29:52 -0700

    underway

--- src/erlative_time.erl
+++ src/erlative_time.erl
@@ -7,7 +7,12 @@
 
 -export([
 
-    since/1
+    epoch/0,
+
+    since/1,
+
+    about/1,
+      about/2
 
 ]).
 
@@ -15,6 +20,53 @@
 
 
 
-since(When) ->
+epoch() ->
 
-    todo.
+    erlang:universaltime_to_posixtime( calendar:now_to_universal_time( now() ) ).
+
+
+
+
+
+since(When) when is_integer(When) ->
+
+    case epoch() - When of
+        Ago     when Ago > 0     -> {Ago,          ago};
+        FromNow when FromNow < 0 -> {-1 * FromNow, from_now};
+        _                        -> now
+    end.
+
+
+
+
+
+about(First, Second) when First > Second -> about_range(First, Second) ++ " before";
+about(First, Second) when First < Second -> about_range(First, Second) ++ " after";
+about(First, Second)                     -> "at the same time".
+
+about(When) ->
+    Second = epoch(),
+    if
+        When > Second -> about_range(When, Second) ++ " ago";
+        When < Second -> about_range(When, Second) ++ " from now";
+        true          -> "now"
+    end.
+
+
+
+
+
+drop_cycle(LastDelta, [{NextRange, NextLabel} | FollowPair], LastLabel) ->
+
+
+
+
+
+about_range(First, Second) ->
+
+     Ranges = [
+         { 1, "second" }
+     ],
+
+     Delta = abs(First - Second),
+     integer_to_list(Delta) ++ " " ++ "seconds".
--- test/erlative_time_tests.erl
+++ test/erlative_time_tests.erl
@@ -5,8 +5,12 @@
 -include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
+
+
+
+
 since_test_() ->
 
-    { "tautology", [
-        { "anything", ?_assert(todo =:= erlative_time:since(1)) }
+    { "unixtime", [
+        { "now", ?_assert(todo =:= erlative_time:since( erlative_time:epoch() )) }
     ] }.
